d444e57980551d00cf6966e37f6ac152ed9e4293
Riley <vishnukey@gmail.com>
2019-06-10 00:14:28 -0600

    Set up ReasonReact project with react-hooks

---
+++ lib/bs/src/Index.mlast.d
@@ -0,0 +1,5 @@
+src/Index-ReactHooksTemplate.cmj :
+src/Component1-ReactHooksTemplate.cmj
+src/Component1-ReactHooksTemplate.cmi
+src/Component2-ReactHooksTemplate.cmj
+src/Component2-ReactHooksTemplate.cmi
